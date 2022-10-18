679ba7ea31d24b22364e07fd89678e826470b310
Pyrotchnst <csundheim@gmail.com>
2022-10-18 15:50:53 -0600

    updated lib

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // require npm packages
 //TODO require files
-
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/renderHTML");
+const { resolve } = require("path");
+const { report } = require("process");
 //write functions
 // promts
 function managerPrompts() {
