d02efe07711d870e2854e195fa9e96b680bd6f57
Pyrotchnst <csundheim@gmail.com>
2022-10-18 15:17:24 -0600

    index.js functions

--- index.js
+++ index.js
@@ -126,3 +126,89 @@ function employeePrompt() {
 }
 
 // make functions
+const allEmployee = [];
+
+function makeEngineer() {
+  engineerPrompt().then(function (response) {
+    const engineer = new Engineer(
+      response.name,
+      response.id,
+      response.email,
+      response.github
+    );
+    if (response.addEmployee == "Engineer") {
+      allEmployee.push(engineer);
+      makeEngineer();
+    } else if (response.addEmployee == "Intern") {
+      allEmployee.push(engineer);
+      makeIntern();
+    } else if (response.addEmployee == "None") {
+      allEmployee.push(engineer);
+
+      fs.writeFile("./output/team.html", render(allEmployee), function (err) {
+        if (err) throw err;
+        console.log("Writing file @makeEngineer function.");
+      });
+
+      return;
+    }
+  });
+}
+
+function makeIntern() {
+  internPrompt().then(function (response) {
+    const intern = new Intern(
+      response.name,
+      response.id,
+      response.email,
+      response.school
+    );
+    if (response.addEmployee == "Engineer") {
+      allEmployee.push(intern);
+      makeEngineer();
+    } else if (response.addEmployee == "Intern") {
+      allEmployee.push(intern);
+      makeIntern();
+    } else if (response.addEmployee == "None") {
+      allEmployee.push(intern);
+
+      fs.writeFile("./output/team.html", render(allEmployee), function (err) {
+        if (err) throw err;
+        console.log("Writing file @makeIntern function.");
+      });
+
+      return;
+    }
+  });
+}
+
+function makeManager() {
+  managerPrompts().then(function (response) {
+    const manager = new Manager(
+      response.name,
+      response.id,
+      response.email,
+      response.officeNumber
+    );
+
+    if (response.addEmployee == "Engineer") {
+      allEmployee.push(manager);
+      makeEngineer();
+    } else if (response.addEmployee == "Intern") {
+      allEmployee.push(manager);
+      makeIntern();
+    } else if (response.addEmployee == "None") {
+      allEmployee.push(manager);
+
+      fs.writeFile("./output/team.html", render(allEmployee), function (err) {
+        if (err) throw err;
+        console.log("Writing file @makeManager function.");
+      });
+
+      return;
+    }
+  });
+}
+
+console.log("Building team.");
+makeManager();
